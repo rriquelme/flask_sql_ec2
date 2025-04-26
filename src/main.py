87deea7d172f9c35745b05e0e8cd32bb7e9d1373
Roberto Riquelme <rers.92@gmail.com>
2025-04-26 17:00:04 +1000

    feat: adding mysql stuff

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
-from flask import Flask
+
+from flask import Flask, request, render_template_string
+import pymysql
 
 app = Flask(__name__)
 
@@ -6,5 +8,32 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+# Database connection config
+db = pymysql.connect(
+    host="localhost",
+    database="mydatabase"
+)
+
+@app.route("/test/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        
+        # Insert into database
+        cursor = db.cursor()
+        sql = "INSERT INTO users (name, email) VALUES (%s, %s)"
+        cursor.execute(sql, (name, email))
+        db.commit()
+        
+        return f"Saved {name} with email {email}!"
+
+    return render_template_string("""
+        <form method="post">
+            Name: <input type="text" name="name"><br>
+            Email: <input type="email" name="email"><br>
+            <input type="submit" value="Submit">
+        </form>
+    """)
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
